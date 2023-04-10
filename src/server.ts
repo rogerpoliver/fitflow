694480c329ed974fe69212945a5b2e6085f06bf2
Roger Oliveira <rogerpolvr@gmail.com>
2023-04-09 23:17:15 -0300

    chore: dotenv config setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { app } from "./app";
-
+import { env } from "./env";
 app
   .listen({
     host: "0.0.0.0",
-    port: 3333,
+    port: env.PORT,
   })
-  .then(() => "🚀 HTTP Server Running!");
+  .then(() => console.log("🚀 HTTP Server Running!"));

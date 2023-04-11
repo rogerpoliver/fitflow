2763c3ff0dc35ec5d85d0edc6d02c13c6cd58757
Roger Oliveira <rogerpolvr@gmail.com>
2023-04-11 00:27:35 -0300

    chore: prisma config setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+void prisma.user.create({
+  data: {
+    name: 'Roger Oliveira',
+    email: 'rogerpolvr@gmail.com'
+  }
+})
